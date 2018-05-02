35dc992e9dbb99934033f40f8b0345fc19a7f55d
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-05-02 12:19:24 +0200

    Update KuponaDE.php

--- src/ResultField/KuponaDE.php
+++ src/ResultField/KuponaDE.php
@@ -120,7 +120,7 @@ class KuponaDE extends ResultFields
         /**
          * @var LanguageMutator $languageMutator
          */
-		$languageMutator = pluginApp(LanguageMutator::class, ['language' => [$settings->get('lang')]]);
+		$languageMutator = pluginApp(LanguageMutator::class, ['languages' => [$settings->get('lang')]]);
 
         // Fields
         $fields = [
